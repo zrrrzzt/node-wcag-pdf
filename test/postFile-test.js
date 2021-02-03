7adaa319a5463beb794cb1ec336f78a7914899a2
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-02-03 19:55:28 +0100

    Changes var to const (patch)

--- test/postFile-test.js
+++ test/postFile-test.js
@@ -1,11 +1,11 @@
 'use strict'
 
-var tap = require('tap')
-var postFile = require('../lib/postFile')
+const tap = require('tap')
+const postFile = require('../lib/postFile')
 
 tap.test('postFile requires a filePath', function (test) {
-  var filePath = false
-  var expectedErrorMessage = 'Missing required input: filePath'
+  const filePath = false
+  const expectedErrorMessage = 'Missing required input: filePath'
   postFile(filePath, function (error, data) {
     tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
     test.done()
@@ -13,7 +13,7 @@ tap.test('postFile requires a filePath', function (test) {
 })
 
 tap.test('postFile returns data', function (test) {
-  var filePath = 'test/data/invalid.pdf'
+  const filePath = 'test/data/invalid.pdf'
   postFile(filePath, function (error, data) {
     if (error) {
       throw error
